f5809c469ae26aa2bffb29b9fb46fce4d1ab8542
AliceEverglade <91664294+AliceEverglade@users.noreply.github.com>
2021-11-12 17:01:07 +0100

    welp 1 thing is fixed...
    now just to stop it from teleporting to y=0

--- The Legend Of Alice/Assets/Scripts/PlayerMovement.cs
+++ The Legend Of Alice/Assets/Scripts/PlayerMovement.cs
@@ -26,7 +26,7 @@ public class PlayerMovement : MonoBehaviour
         {
             float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-            transform.rotation = Quaternion.Euler(0f,targetAngle,0f);
+            transform.rotation = Quaternion.Euler(0f,angle,0f);
 
             Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
